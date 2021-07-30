ea3864e6a2fcd579d94da19a2896955e797d8a58
PratikGarai <pratikgarai0208@gmail.com>
2021-07-30 08:25:41 +0530

    some more routes

--- main.py
+++ main.py
@@ -4,4 +4,26 @@ app = FastAPI()
 
 @app.get('/')
 def index(): 
-    return "Hello world!"
+    return {
+        "status" : "Success", 
+        "message " : "Hello world!"
+    }
+
+@app.get('/blog/unpublished')
+def getUnpublished():
+    return {
+        "data" : "All unpublished"
+    }
+
+@app.get('/blog/{id}')
+def singleBlog(id : int):
+    return {
+        "id" : id
+    }
+
+@app.get('/blog/{id1}/comments/{id2}')
+def singleBlogComment(id1 : int, id2):
+    return {
+        "id1" : id1,
+        "id2" : id2
+    }
