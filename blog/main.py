f4ed9cc4d75a41e1a1d7f509b28c530475164a18
PratikGarai <pratikgarai0208@gmail.com>
2021-08-04 01:05:53 +0530

    single blog fetcher

--- blog/main.py
+++ blog/main.py
@@ -32,6 +32,10 @@ def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
+@app.get('/blog/{id}')
+def get_single_blog(id:int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    return blog
 
 if __name__=="__main__":
     uvicorn.run(app, host="127.0.0.1", port="8000")
